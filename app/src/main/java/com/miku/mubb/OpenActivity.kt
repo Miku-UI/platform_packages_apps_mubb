2f1c3b709d948c4e9d437ea48009b336ebf919ed
AmeChanRain <downloadbot007@gmail.com>
2022-12-02 10:34:27 +0800

    MUBB: Remove unnecessary immutable variable

--- app/src/main/java/com/miku/mubb/OpenActivity.kt
+++ app/src/main/java/com/miku/mubb/OpenActivity.kt
@@ -7,7 +7,6 @@ import com.miku.mubb.utils.LogRecorder
 class OpenActivity : Activity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        val intent = intent
         val url = intent.data.toString()
         LogRecorder.d(TAG, "Prepare to start MUUB as Built-in Browser, url is $url")
         MainActivity.startActivity(this, url)
